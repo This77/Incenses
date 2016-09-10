fe8d473b00ba55c16d1908617397f0818400d609
This77 <gorilla@sunrise.ch>
2016-09-10 11:57:20 +0200

    Homework via git.

--- rauch/models.py
+++ rauch/models.py
@@ -1,25 +1,24 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
 
-
-	
 class Eigenschaft(models.Model):
-	"""Repraesentiert eine Eigenschaft"""
-	bezeichnung = models.CharField(max_length=200)
-	
-	def __str__(self):
-		return self.bezeichnung
-	
+    """Repraesentiert eine Eigenschaft"""
+    bezeichnung = models.CharField(max_length=256)
+
+    def __str__(self):
+        return '{0}({1})'.format(self.bezeichnung, str(self.id))
+
 class Substanz(models.Model):
-	"""Repraesentiert eine Substanz"""
-	bezeichnung = models.CharField(max_length=200)
-	name_botanisch = models.CharField(max_length=200)
-	eigenschaften = models.ManyToManyField(Eigenschaft)
-	kaufdatum = models.DateTimeField('Kaufdatum')
-	preispergramm = models.IntegerField('Preis pro Gramm', default=0)	
-	
-	def __str__(self):
-		return self.bezeichnung
+    """Repraesentiert eine Substanz"""
+    bezeichnung = models.CharField(max_length=256)
+    aka = models.CharField(max_length=1024, blank=True, null=True)
+    name_botanisch = models.CharField(max_length=256)
+    herkunft = models.CharField(max_length=1024, blank=True, null=True)
+    tradition = models.CharField(max_length=2048, blank=True, null=True)
+    eigenschaft_set = models.ManyToManyField(Eigenschaft)
+    #kaufdatum = models.DateTimeField('Kaufdatum', blank=True, null=True, default=None)
+    preispergramm = models.IntegerField('Preis pro Gramm', default=0)
+
+    def __str__(self):
+        return '{0}({1})'.format(self.bezeichnung, str(self.id))
